164d4c0734f8f24bc8897d005eb6723d691d3aa5
amanda-w <amandanwong@gmail.com>
2022-10-01 10:28:36 -0700

    add PUT route and edit.ejs

--- server.js
+++ server.js
@@ -51,9 +51,45 @@ app.get('/profile', isLoggedIn, (req, res) => {
   res.render('profile', { id, name, email });
 });
 
+app.get('/profile/edit', isLoggedIn, (req, res) => {
+  res.render('edit');
+});
+
 // Other Routes
 app.use('/recipes', require('./controllers/recipes'));
 //app.use('/meal_plan', require('./controllers/meal_plan'));
+
+// Put Route
+app.put('/profile/:id', isLoggedIn, async (req, res) => {
+  try {
+      const foundUser = await db.user.findOne({ where: { email: req.body.email }});
+      if (foundUser.email && foundUser.id !== req.user.id) {
+        req.flash('error', 'Email already exists. Please try again.');
+        res.redirect('/profile');
+      } else {
+        const usersUpdated = await db.user.update({
+          email: req.body.email,
+          name: req.body.name
+        }, {
+          where: {
+            id: req.params.id
+          }
+        });
+
+        console.log('********** PUT ROUTE *************');
+        console.log('Users updated', usersUpdated);
+        console.log('**************************************************');
+  
+        // redirect back to the profile page
+        res.redirect('/profile'); // route
+      }
+  } catch (error) {
+    console.log('*********************ERROR***********************');
+    console.log(error);
+    console.log('**************************************************');
+    res.render('edit');
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
